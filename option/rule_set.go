ba3b9f20c6195b7b96fcdea09aa8fbe95213fe78
PuerNya <safarier@outlook.com>
2024-08-13 06:42:47 +0800

    add rule-provider clash-api

--- option/rule_set.go
+++ option/rule_set.go
@@ -98,7 +98,7 @@ func (r *RuleSet) UnmarshalJSON(bytes []byte) error {
 			return E.New("unknown rule-set format: " + r.Format)
 		}
 	} else {
-		r.Format = ""
+		r.Format = C.RuleSetFormatSource
 		r.Path = ""
 	}
 	if r.Type == C.RuleSetTypeRemote && r.Path != "" && r.RemoteOptions.InitialPath != "" {
