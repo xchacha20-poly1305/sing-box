97d4d900fc3648ea7136be43826d9179ed9b3c4f
reF1nd <ref1nd@thunder-cloud.cc>
2025-03-19 17:42:39 +0800

    Restore proxy protocol

--- option/direct.go
+++ option/direct.go
@@ -16,12 +16,11 @@ type DirectInboundOptions struct {
 
 type _DirectOutboundOptions struct {
 	DialerOptions
+	ProxyProtocol uint8 `json:"proxy_protocol,omitempty" enum:"0,1,2"`
 	// Deprecated: Use Route Action instead
 	OverrideAddress string `json:"override_address,omitempty" schema:"omit"`
 	// Deprecated: Use Route Action instead
 	OverridePort uint16 `json:"override_port,omitempty" schema:"omit"`
-	// Deprecated: removed
-	ProxyProtocol uint8 `json:"proxy_protocol,omitempty" schema:"omit"`
 }
 
 type DirectOutboundOptions _DirectOutboundOptions
