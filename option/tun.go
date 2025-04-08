bf37f79a51c9adeba9c375d49e2e73923574655c
reF1nd <ref1nd@thunder-cloud.cc>
2025-04-08 17:50:29 +0800

    Add `auto_redirect_disable_mark_mode` option for tun inbound

--- option/tun.go
+++ option/tun.go
@@ -18,6 +18,7 @@ type TunInboundOptions struct {
 	IPRoute2TableIndex            int                              `json:"iproute2_table_index,omitempty"`
 	IPRoute2RuleIndex             int                              `json:"iproute2_rule_index,omitempty"`
 	AutoRedirect                  bool                             `json:"auto_redirect,omitempty"`
+	AutoRedirectDisableMarkMode   bool                             `json:"auto_redirect_disable_mark_mode,omitempty"`
 	AutoRedirectInputMark         FwMark                           `json:"auto_redirect_input_mark,omitempty"`
 	AutoRedirectOutputMark        FwMark                           `json:"auto_redirect_output_mark,omitempty"`
 	AutoRedirectResetMark         FwMark                           `json:"auto_redirect_reset_mark,omitempty"`
