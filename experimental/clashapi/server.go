df161b16b206828e832b0567595a91f729d13b9b
yelnoo <t50500411@163.com>
2025-04-14 07:12:17 +0800

    clash-api: Add restart support

--- experimental/clashapi/server.go
+++ experimental/clashapi/server.go
@@ -150,6 +150,10 @@ func NewServer(ctx context.Context, logFactory log.ObservableFactory, options op
 		r.Mount("/cache", cacheRouter(ctx))
 		r.Mount("/dns", dnsRouter(s.dnsRouter))
 
+		if service.FromContext[adapter.PlatformInterface](ctx) == nil {
+			r.Mount("/restart", restartRouter(ctx, logFactory))
+		}
+
 		s.setupMetaAPI(r)
 	})
 	if options.ExternalUI != "" {
