ee19d024ce817101fd0034b227d9a912fe610d1e
reF1nd <ref1nd@thunder-cloud.cc>
2025-04-18 19:59:02 +0800

    makes remote rule-set `path` correct

--- option/rule_set_test.go
+++ option/rule_set_test.go
@@ -20,3 +20,44 @@ func TestRemoteRuleSetPathConflict(t *testing.T) {
 	}`), &ruleSet)
 	require.ErrorContains(t, err, "path and initial_path are mutually exclusive")
 }
+
+func TestRemoteRuleSetMultipleTagsPath(t *testing.T) {
+	t.Parallel()
+
+	for _, testCase := range []struct {
+		name         string
+		path         string
+		requireError bool
+	}{
+		{
+			name: "empty",
+		},
+		{
+			name: "placeholder",
+			path: "{tag}.json",
+		},
+		{
+			name:         "shared",
+			path:         "shared.json",
+			requireError: true,
+		},
+	} {
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			content := `{
+				"type": "remote",
+				"tag": ["a", "b"],
+				"format": "source",
+				"path": "` + testCase.path + `",
+				"url": "https://example.com/{tag}.json"
+			}`
+			var ruleSet RuleSet
+			err := json.Unmarshal([]byte(content), &ruleSet)
+			if testCase.requireError {
+				require.ErrorContains(t, err, "missing {tag} placeholder in path")
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
