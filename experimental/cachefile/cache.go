be36118b172afc9fd0762df531c9b49e2e235ecf
reF1nd <ref1nd@thunder-cloud.cc>
2025-04-18 17:19:48 +0800

    Add `path` option for remote provider

--- experimental/cachefile/cache.go
+++ experimental/cachefile/cache.go
@@ -19,11 +19,12 @@ import (
 )
 
 var (
-	bucketSelected   = []byte("selected")
-	bucketExpand     = []byte("group_expand")
-	bucketMode       = []byte("clash_mode")
-	bucketRuleSet    = []byte("rule_set")
-	bucketExternalUI = []byte("external_ui")
+	bucketSelected         = []byte("selected")
+	bucketExpand           = []byte("group_expand")
+	bucketMode             = []byte("clash_mode")
+	bucketRuleSet          = []byte("rule_set")
+	bucketExternalUI       = []byte("external_ui")
+	bucketOutboundProvider = []byte("outbound_provider")
 
 	bucketNameList = []string{
 		string(bucketSelected),
@@ -31,6 +32,7 @@ var (
 		string(bucketMode),
 		string(bucketRuleSet),
 		string(bucketExternalUI),
+		string(bucketOutboundProvider),
 		string(bucketRDRC),
 	}
 
@@ -398,7 +400,7 @@ func (c *CacheFile) SaveExternalUI(tag string, info *adapter.SavedBinary) error 
 func (c *CacheFile) LoadSubscription(tag string) *adapter.SavedBinary {
 	var savedSet adapter.SavedBinary
 	err := c.DB.View(func(t *bbolt.Tx) error {
-		bucket := c.bucket(t, bucketRuleSet)
+		bucket := c.bucket(t, bucketOutboundProvider)
 		if bucket == nil {
 			return os.ErrNotExist
 		}
@@ -416,7 +418,7 @@ func (c *CacheFile) LoadSubscription(tag string) *adapter.SavedBinary {
 
 func (c *CacheFile) SaveSubscription(tag string, sub *adapter.SavedBinary) error {
 	return c.DB.Batch(func(t *bbolt.Tx) error {
-		bucket, err := c.createBucket(t, bucketRuleSet)
+		bucket, err := c.createBucket(t, bucketOutboundProvider)
 		if err != nil {
 			return err
 		}
