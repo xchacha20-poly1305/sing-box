56ccf9853ffe32f15ce8707fdb934b3bbf58c755
PuerNya <safarier@outlook.com>
2025-05-06 03:53:32 +0800

    Support `GET` method for doh

--- protocol/tailscale/dns_transport.go
+++ protocol/tailscale/dns_transport.go
@@ -185,12 +185,12 @@ func (t *DNSTransport) createResolver(directDialer func() N.Dialer, resolver *dn
 				Enabled: true,
 				ALPN:    []string{http2.NextProtoTLS, "http/1.1"},
 			}))
-			return transport.NewHTTPSRaw(t.TransportAdapter, t.logger, myDialer, serverURL, http.Header{}, serverAddr, tlsConfig), nil
+			return transport.NewHTTPSRaw(t.TransportAdapter, t.logger, myDialer, serverURL, http.MethodPost, http.Header{}, serverAddr, tlsConfig), nil
 		case "http":
 			if serverAddr.Port == 0 {
 				serverAddr.Port = 80
 			}
-			return transport.NewHTTPSRaw(t.TransportAdapter, t.logger, myDialer, serverURL, http.Header{}, serverAddr, nil), nil
+			return transport.NewHTTPSRaw(t.TransportAdapter, t.logger, myDialer, serverURL, http.MethodPost, http.Header{}, serverAddr, nil), nil
 		}
 	}
 	serverAddr := M.ParseSocksaddr(resolver.Addr)
