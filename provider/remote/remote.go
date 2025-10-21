b48672a8c405f7eca7df565f6a1704c750242165
xireiki <i@xireiki.com>
2025-10-22 01:12:57 +0800

    Support override TLS option

--- provider/remote/remote.go
+++ provider/remote/remote.go
@@ -69,6 +69,7 @@ type ProviderRemote struct {
 	include           *regexp.Regexp
 
 	overrideDialer *option.OverrideDialerOptions
+	overrideTLS    *option.OverrideTLSOptions
 }
 
 func NewProviderRemote(ctx context.Context, router adapter.Router, logFactory log.Factory, tag string, options option.ProviderRemoteOptions) (adapter.Provider, error) {
@@ -146,6 +147,7 @@ func NewProviderRemote(ctx context.Context, router adapter.Router, logFactory lo
 		include:           (*regexp.Regexp)(options.Include),
 
 		overrideDialer: options.OverrideDialer,
+		overrideTLS:    options.OverrideTLS,
 
 		//nolint:staticcheck
 		downloadDetour: options.DownloadDetour,
@@ -531,7 +533,7 @@ func (s *ProviderRemote) saveCacheFile(hasInfo bool, info adapter.SubscriptionIn
 }
 
 func (s *ProviderRemote) updateProviderFromContent(content string) error {
-	outboundOpts, endpointOpts, err := parser.ParseSubscription(s.ctx, content, s.overrideDialer, s.Tag())
+	outboundOpts, endpointOpts, err := parser.ParseSubscription(s.ctx, content, s.overrideDialer, s.overrideTLS, s.Tag())
 	if err != nil {
 		return err
 	}
