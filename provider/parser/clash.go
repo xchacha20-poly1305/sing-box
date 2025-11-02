7db6f4f1d3c80322e15123bafe37bc6e8f0e28e0
reF1nd <ref1nd@thunder-cloud.cc>
2025-11-02 19:10:30 +0800

    provider: Fix clash `tls` parser

--- provider/parser/clash.go
+++ provider/parser/clash.go
@@ -785,7 +785,7 @@ type TLSOptions struct {
 }
 
 func (t *TLSOptions) Build() *option.OutboundTLSOptions {
-	if t == nil {
+	if t == nil || !t.TLS {
 		return nil
 	}
 	return &option.OutboundTLSOptions{
