ef7126c31cdab078df973a61cde95cd1e68232d9
reF1nd <ref1nd@thunder-cloud.cc>
2025-12-05 00:59:26 +0800

    provider: Migrate to shared HTTP client transport

--- protocol/group/urltest.go
+++ protocol/group/urltest.go
@@ -191,6 +191,13 @@ func (s *URLTest) InterfaceUpdated() {
 	go group.CheckOutbounds(true)
 }
 
+func (s *URLTest) isGroupActive() bool {
+	if !s.group.started {
+		return false
+	}
+	return time.Since(s.group.lastActive.Load()) <= s.group.idleTimeout
+}
+
 func (s *URLTest) DialContext(ctx context.Context, network string, destination M.Socksaddr) (net.Conn, error) {
 	s.group.Touch()
 	var outbound adapter.Outbound
@@ -288,17 +295,19 @@ func (s *URLTest) onProviderUpdated(tag string) error {
 		outbounds = append(outbounds, detour)
 	}
 	s.tags, s.group.outbounds = tags, outbounds
-	s.group.access.Lock()
-	if s.group.ticker != nil {
-		s.group.ticker.Reset(s.group.interval)
+	if s.isGroupActive() {
+		s.group.access.Lock()
+		if s.group.ticker != nil {
+			s.group.ticker.Reset(s.group.interval)
+		}
+		s.group.access.Unlock()
+		ctx, cancel := context.WithCancel(s.ctx)
+		if s.cancel != nil {
+			s.cancel()
+		}
+		s.cancel = cancel
+		s.URLTest(ctx)
 	}
-	s.group.access.Unlock()
-	ctx, cancel := context.WithCancel(s.ctx)
-	if s.cancel != nil {
-		s.cancel()
-	}
-	s.cancel = cancel
-	s.URLTest(ctx)
 	return nil
 }
 
