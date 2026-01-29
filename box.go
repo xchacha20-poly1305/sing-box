0aba8e2f73a7e1056004c4853b099943cd8d17f2
reF1nd <ref1nd@thunder-cloud.cc>
2026-01-29 20:06:44 +0800

    Start DNS transports before providers
    Move provider startup after DNS transport and router initialization
    in the Start stage. This ensures DNS resolution is available when
    remote providers fetch subscriptions during startup.

--- box.go
+++ box.go
@@ -579,7 +579,7 @@ func (s *Box) preStart() error {
 	if err != nil {
 		return err
 	}
-	err = adapter.Start(s.logger, adapter.StartStateStart, s.outbound, s.provider, s.dnsTransport, s.network, s.connection)
+	err = adapter.Start(s.logger, adapter.StartStateStart, s.outbound, s.dnsTransport, s.network, s.connection)
 	if err != nil {
 		return err
 	}
@@ -587,7 +587,7 @@ func (s *Box) preStart() error {
 	if err != nil {
 		return err
 	}
-	err = adapter.Start(s.logger, adapter.StartStateStart, s.router, s.dnsRouter)
+	err = adapter.Start(s.logger, adapter.StartStateStart, s.provider, s.router, s.dnsRouter)
 	if err != nil {
 		return err
 	}
