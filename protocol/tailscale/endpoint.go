49a19c9d7e127b9b9ed72298c6634910b8d8b4b8
reF1nd <ref1nd@thunder-cloud.cc>
2026-02-06 22:01:40 +0800

    Add configurable `gso` option for WireGuard and Tailscale endpoint

--- protocol/tailscale/endpoint.go
+++ protocol/tailscale/endpoint.go
@@ -31,7 +31,7 @@ import (
 	"github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/option"
 	"github.com/sagernet/sing-box/route/rule"
-	"github.com/sagernet/sing-tun"
+	tun "github.com/sagernet/sing-tun"
 	"github.com/sagernet/sing-tun/ping"
 	"github.com/sagernet/sing/common"
 	"github.com/sagernet/sing/common/bufio"
@@ -112,6 +112,7 @@ type Endpoint struct {
 
 	systemInterface     bool
 	systemInterfaceName string
+	systemInterfaceGSO  bool
 	systemInterfaceMTU  uint32
 	serverStarted       bool
 	started             atomic.Bool
@@ -189,6 +190,10 @@ func NewEndpoint(ctx context.Context, router adapter.Router, logger log.ContextL
 	} else {
 		udpTimeout = C.UDPTimeout
 	}
+	gso := options.SystemInterface
+	if options.SystemInterfaceGSO != nil {
+		gso = *options.SystemInterfaceGSO
+	}
 	var remoteIsDomain bool
 	if options.ControlURL != "" {
 		controlURL, err := url.Parse(options.ControlURL)
@@ -263,6 +268,7 @@ func NewEndpoint(ctx context.Context, router adapter.Router, logger log.ContextL
 		udpTimeout:                 udpTimeout,
 		icmpTimeout:                C.ICMPTimeout,
 		systemInterface:            options.SystemInterface,
+		systemInterfaceGSO:         gso,
 		systemInterfaceName:        options.SystemInterfaceName,
 		systemInterfaceMTU:         options.SystemInterfaceMTU,
 	}, nil
@@ -316,7 +322,7 @@ func (t *Endpoint) start() error {
 		tunOptions := tun.Options{
 			Name:                      tunName,
 			MTU:                       mtu,
-			GSO:                       true,
+			GSO:                       t.systemInterfaceGSO,
 			InterfaceScope:            true,
 			InterfaceMonitor:          t.network.InterfaceMonitor(),
 			InterfaceFinder:           t.network.InterfaceFinder(),
--- transport/wireguard/endpoint_options.go
+++ transport/wireguard/endpoint_options.go
@@ -5,7 +5,7 @@ import (
 	"net/netip"
 	"time"
 
-	"github.com/sagernet/sing-tun"
+	tun "github.com/sagernet/sing-tun"
 	"github.com/sagernet/sing/common/logger"
 	M "github.com/sagernet/sing/common/metadata"
 	N "github.com/sagernet/sing/common/network"
@@ -15,6 +15,7 @@ type EndpointOptions struct {
 	Context      context.Context
 	Logger       logger.ContextLogger
 	System       bool
+	GSO          bool
 	Handler      tun.Handler
 	UDPTimeout   time.Duration
 	ICMPTimeout  time.Duration
