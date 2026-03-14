75206ae1790f6088ccbf68622ebc47e504fe2f66
reF1nd <ref1nd@thunder-cloud.cc>
2026-03-14 23:19:58 +0800

    provider: Add endpoint support

--- provider/parser/parser.go
+++ provider/parser/parser.go
@@ -7,21 +7,21 @@ import (
 	E "github.com/sagernet/sing/common/exceptions"
 )
 
-var subscriptionParsers = []func(ctx context.Context, content string) ([]option.Outbound, error){
+var subscriptionParsers = []func(ctx context.Context, content string) ([]option.Outbound, []option.Endpoint, error){
 	ParseBoxSubscription,
 	ParseClashSubscription,
 	ParseSIP008Subscription,
 	ParseRawSubscription,
 }
 
-func ParseSubscription(ctx context.Context, content string) ([]option.Outbound, error) {
+func ParseSubscription(ctx context.Context, content string) ([]option.Outbound, []option.Endpoint, error) {
 	var pErr error
 	for _, parser := range subscriptionParsers {
-		servers, err := parser(ctx, content)
-		if len(servers) > 0 {
-			return servers, nil
+		outbounds, endpoints, err := parser(ctx, content)
+		if len(outbounds) > 0 || len(endpoints) > 0 {
+			return outbounds, endpoints, nil
 		}
 		pErr = E.Errors(pErr, err)
 	}
-	return nil, E.Cause(pErr, "no servers found")
+	return nil, nil, E.Cause(pErr, "no servers found")
 }
