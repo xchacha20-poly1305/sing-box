017444879ddb5faa7fcbb4e6fcbfa0b1dbe68d31
reF1nd <ref1nd@thunder-cloud.cc>
2026-04-17 16:57:26 +0800

    external-ui: Migrate to shared HTTP client transport

--- experimental/clashapi/server.go
+++ experimental/clashapi/server.go
@@ -21,6 +21,7 @@ import (
 	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/experimental"
 	"github.com/sagernet/sing-box/experimental/clashmode"
+	"github.com/sagernet/sing-box/experimental/deprecated"
 	"github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/option"
 	"github.com/sagernet/sing/common"
@@ -59,6 +60,7 @@ type Server struct {
 	externalUI                string
 	externalUIDownloadURL     string
 	externalUIDownloadURLHash [32]byte
+	externalUIHTTPClient      *option.HTTPClientOptions
 	externalUIDownloadDetour  string
 	externalUIUpdateInterval  time.Duration
 	cacheFile                 adapter.CacheFile
@@ -110,9 +112,12 @@ func NewServer(ctx context.Context, logFactory log.ObservableFactory, options op
 		externalController:        options.ExternalController != "",
 		externalUIDownloadURL:     downloadURL,
 		externalUIDownloadURLHash: sha256.Sum256([]byte(downloadURL)),
-		externalUIDownloadDetour:  options.ExternalUIDownloadDetour,
+		externalUIHTTPClient:      options.ExternalUIHTTPClient,
 		externalUIUpdateInterval:  updateInterval,
 		cacheFile:                 service.FromContext[adapter.CacheFile](ctx),
+
+		//nolint:staticcheck
+		externalUIDownloadDetour: options.ExternalUIDownloadDetour,
 	}
 	//goland:noinspection GoDeprecation
 	//nolint:staticcheck
@@ -169,10 +174,15 @@ func (s *Server) Name() string {
 }
 
 func (s *Server) Start(stage adapter.StartStage) error {
-	if stage != adapter.StartStateStarted {
-		return nil
-	}
-	if s.externalController {
+	switch stage {
+	case adapter.StartStateStart:
+		if s.externalUIDownloadDetour != "" && (s.externalUIHTTPClient == nil || s.externalUIHTTPClient.IsEmpty()) {
+			deprecated.Report(s.ctx, deprecated.OptionLegacyClashAPIExternalUIDownloadDetour)
+		}
+	case adapter.StartStateStarted:
+		if !s.externalController {
+			break
+		}
 		s.ctx, s.updateCancel = context.WithCancel(s.ctx)
 		var forceUpdate bool
 		if s.externalUI != "" && s.cacheFile != nil {
