d58c4f2936c02c62e5fb434520c35e5b079154fa
reF1nd <ref1nd@thunder-cloud.cc>
2026-07-10 21:16:02 +0800

    Add `domain_match_strategy` and `default_domain_match_strategy`

--- constant/rule.go
+++ constant/rule.go
@@ -48,3 +48,13 @@ const (
 	RuleActionRejectMethodDrop    = "drop"
 	RuleActionRejectMethodReply   = "reply"
 )
+
+type DomainMatchStrategy = uint8
+
+const (
+	DomainMatchStrategyAsIS DomainMatchStrategy = iota
+	DomainMatchStrategyPreferFQDN
+	DomainMatchStrategyPreferSniffHost
+	DomainMatchStrategyFQDNOnly
+	DomainMatchStrategySniffHostOnly
+)
