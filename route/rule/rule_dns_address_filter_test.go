6ddaf2c3b65aff773bc5c885fdec1f871e9cb843
reF1nd <ref1nd@thunder-cloud.cc>
2026-07-10 21:16:02 +0800

    Add `domain_match_strategy` and `default_domain_match_strategy`

--- route/rule/rule_dns_address_filter_test.go
+++ route/rule/rule_dns_address_filter_test.go
@@ -27,6 +27,10 @@ func (r *addressFilterRouter) RuleSet(tag string) (adapter.RuleSet, bool) {
 	return ruleSet, loaded
 }
 
+func (r *addressFilterRouter) DefaultDomainMatchStrategy() C.DomainMatchStrategy {
+	return C.DomainMatchStrategyAsIS
+}
+
 func addressFilterContext(t *testing.T, ruleSetConfigs map[string]string) context.Context {
 	t.Helper()
 	router := &addressFilterRouter{ruleSets: make(map[string]adapter.RuleSet)}
--- route/rule/rule_item_adguard.go
+++ route/rule/rule_item_adguard.go
@@ -4,35 +4,58 @@ import (
 	"strings"
 
 	"github.com/sagernet/sing-box/adapter"
+	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing/common/domain"
 )
 
 var _ RuleItem = (*AdGuardDomainItem)(nil)
 
 type AdGuardDomainItem struct {
-	matcher *domain.AdGuardMatcher
+	matcher             *domain.AdGuardMatcher
+	domainMatchStrategy C.DomainMatchStrategy
 }
 
-func NewAdGuardDomainItem(ruleLines []string) *AdGuardDomainItem {
+func NewAdGuardDomainItem(ruleLines []string, domainMatchStrategy C.DomainMatchStrategy) *AdGuardDomainItem {
 	return &AdGuardDomainItem{
 		domain.NewAdGuardMatcher(ruleLines),
+		domainMatchStrategy,
 	}
 }
 
-func NewRawAdGuardDomainItem(matcher *domain.AdGuardMatcher) *AdGuardDomainItem {
+func NewRawAdGuardDomainItem(matcher *domain.AdGuardMatcher, domainMatchStrategy C.DomainMatchStrategy) *AdGuardDomainItem {
 	return &AdGuardDomainItem{
 		matcher,
+		domainMatchStrategy,
 	}
 }
 
 func (r *AdGuardDomainItem) Match(metadata *adapter.InboundContext) bool {
 	var domainHost string
-	if metadata.SniffHost != "" {
-		domainHost = metadata.SniffHost
-	} else if metadata.Destination.IsDomain() {
-		domainHost = metadata.Destination.Fqdn
-	} else {
-		domainHost = metadata.Domain
+	switch r.domainMatchStrategy {
+	case C.DomainMatchStrategyPreferFQDN:
+		if metadata.Destination.IsDomain() {
+			domainHost = metadata.Destination.Fqdn
+		} else if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		} else {
+			domainHost = metadata.Domain
+		}
+	case C.DomainMatchStrategyFQDNOnly:
+		if metadata.Destination.IsDomain() {
+			domainHost = metadata.Destination.Fqdn
+		}
+	case C.DomainMatchStrategySniffHostOnly:
+		if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		}
+	default:
+		if metadata.SniffHost != "" {
+			domainHost = metadata.SniffHost
+		} else if metadata.Destination.IsDomain() {
+			domainHost = metadata.Destination.Fqdn
+		} else {
+			domainHost = metadata.Domain
+		}
 	}
 	if domainHost == "" {
 		return false
