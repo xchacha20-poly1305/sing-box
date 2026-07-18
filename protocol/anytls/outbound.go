a77b60f3046c5787dfe08ca7d2538810f6106ed3
anytls <anytls>
2026-07-18 10:02:52 +0900

    Update anytls v0.0.13

--- protocol/anytls/outbound.go
+++ protocol/anytls/outbound.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net"
 	"os"
+	"strings"
 
 	"github.com/sagernet/sing-box/adapter"
 	"github.com/sagernet/sing-box/adapter/outbound"
@@ -19,10 +20,15 @@ import (
 
 	anytls "github.com/anytls/sing-anytls"
 	"github.com/anytls/sing-anytls/session"
+	"github.com/anytls/sing-anytls/util"
 )
 
 func RegisterOutbound(registry *outbound.Registry) {
 	outbound.Register[option.AnyTLSOutboundOptions](registry, C.TypeAnyTLS, NewOutbound)
+
+	if !strings.Contains(util.Version, "sing-box") {
+		util.Version = util.Version + " sing-box/" + C.Version
+	}
 }
 
 var _ adapter.OutboundWithMultiplex = (*Outbound)(nil)
@@ -71,6 +77,7 @@ func NewOutbound(ctx context.Context, router adapter.Router, logger log.ContextL
 		IdleSessionCheckInterval: options.IdleSessionCheckInterval.Build(),
 		IdleSessionTimeout:       options.IdleSessionTimeout.Build(),
 		MinIdleSession:           options.MinIdleSession,
+		DisableReuse:             options.DisableReuse,
 		DialOut:                  outbound.dialOut,
 		Logger:                   logger,
 	})
