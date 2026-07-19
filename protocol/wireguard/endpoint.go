80b0dd4201dccc1e40c7c8fb62a46bdc91c0ae27
reF1nd <ref1nd@thunder-cloud.cc>
2026-07-20 02:14:41 +0800

    Recover WireGuard cleanly after network loss
    Clarify WireGuard bind and packet-listen error messages.

--- protocol/wireguard/endpoint.go
+++ protocol/wireguard/endpoint.go
@@ -179,7 +179,7 @@ func (w *Endpoint) InterfaceUpdated() {
 	}
 	err := w.endpoint.BindUpdate()
 	if err != nil {
-		w.logger.Error(E.Cause(err, "update bind"))
+		w.logger.Error(E.Cause(err, "update WireGuard bind after network change"))
 	}
 }
 
