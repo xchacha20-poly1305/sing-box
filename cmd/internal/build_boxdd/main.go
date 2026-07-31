a2619f14eaab4fc5c0c8946515afc6d13fd5744d
reF1nd <ref1nd@thunder-cloud.cc>
2026-07-31 19:59:18 +0800

    Enable eBPF for SFL builds

--- cmd/internal/build_boxdd/main.go
+++ cmd/internal/build_boxdd/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/sagernet/sing-box/cmd/internal/build_shared"
 	"github.com/sagernet/sing-box/log"
 	E "github.com/sagernet/sing/common/exceptions"
+	"github.com/sagernet/sing/common/shell"
 )
 
 var (
@@ -62,6 +63,13 @@ func build() error {
 	tags, err := buildTags(operatingSystem, architecture, cgoEnabled)
 	if err != nil {
 		return err
+	}
+	if operatingSystem == "linux" && cgoEnabled {
+		tags = append(tags, "with_ebpf")
+		err = shell.Exec("make", "-C", "common/ebpf", "generate").Attach().Run()
+		if err != nil {
+			return E.Cause(err, "generate eBPF object")
+		}
 	}
 	arguments := []string{
 		"build",
