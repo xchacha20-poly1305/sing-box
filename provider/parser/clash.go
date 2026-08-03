78f24f8133a3c790e55bbc699d3d8cc9d5dd92c0
reF1nd <ref1nd@thunder-cloud.cc>
2026-08-04 00:00:27 +0800

    Support AnyTLS provider overrides

--- provider/parser/clash.go
+++ provider/parser/clash.go
@@ -525,6 +525,7 @@ type AnyTLSOption struct {
 	IdleSessionCheckInterval int    `yaml:"idle-session-check-interval,omitempty"`
 	IdleSessionTimeout       int    `yaml:"idle-session-timeout,omitempty"`
 	MinIdleSession           int    `yaml:"min-idle-session,omitempty"`
+	DisableReuse             bool   `yaml:"disable-reuse,omitempty"`
 }
 
 func (a *AnyTLSOption) Build() any {
@@ -537,6 +538,7 @@ func (a *AnyTLSOption) Build() any {
 		IdleSessionCheckInterval:    badoption.Duration(a.IdleSessionCheckInterval),
 		IdleSessionTimeout:          badoption.Duration(a.IdleSessionTimeout),
 		MinIdleSession:              a.MinIdleSession,
+		DisableReuse:                a.DisableReuse,
 	}
 }
 
