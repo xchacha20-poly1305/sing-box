0c5a3ff7aa1b182784e787c111b6e9100f8fb1a6
reF1nd <ref1nd@thunder-cloud.cc>
2026-08-06 19:14:02 +0800

    provider: Fix VMess and Hysteria 2 link option parsing

---
+++ provider/parser/link_test.go
@@ -0,0 +1,29 @@
+package parser
+
+import (
+	"encoding/base64"
+	"testing"
+
+	"github.com/sagernet/sing-box/option"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseVMessLinkSNI(t *testing.T) {
+	link := "vmess://" + base64.RawURLEncoding.EncodeToString([]byte(`{"add":"192.0.2.1","port":"443","id":"11111111-1111-1111-1111-111111111111","tls":"tls","sni":"example.com"}`))
+	outbound, err := ParseSubscriptionLink(link)
+	require.NoError(t, err)
+
+	options := outbound.Options.(*option.VMessOutboundOptions)
+	require.Equal(t, "192.0.2.1", options.Server)
+	require.Equal(t, "example.com", options.TLS.ServerName)
+}
+
+func TestParseHysteria2LinkOptions(t *testing.T) {
+	outbound, err := ParseSubscriptionLink("hysteria2://password@192.0.2.1:443?sni=example.com&pinSHA256=AA:BB&mport=40000-50000")
+	require.NoError(t, err)
+
+	options := outbound.Options.(*option.Hysteria2OutboundOptions)
+	require.Equal(t, []string{"40000:50000"}, []string(options.ServerPorts))
+	require.Equal(t, "example.com", options.TLS.ServerName)
+	require.Equal(t, "AA:BB", options.TLS.CertificatePinSHA256)
+}
